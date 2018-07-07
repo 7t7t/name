959f5b2a414f46712039e7341ce9dfcae6d55ec4
7t7t <395709574@qq.com>
2018-07-07 15:38:13 +0800

    add_out_put

--- pages/person/person.js
+++ pages/person/person.js
@@ -5,7 +5,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    icons: {
+      firstName: '/img/foo.png',
+      lastName: '/img/foo.png'
+    },
+    texts: '夕云可麦言米舍里沐风充艾丝竹延栈巧乔禾听安予蔓临深望庭巡',
+    firstWord: '黄',
+    secondWord: '云',
+    thirdWord: '夕',
+    names: [],
+    showList: false
   },
 
   /**
@@ -62,5 +71,61 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+  /**
+   * 用户离开点击后设值到data
+   */
+  setTexts: function (e) {
+    let that = this;
+    let id = e.currentTarget.id;
+    this.setData({
+      [id]:  e.detail.value
+    })
+  },
+  /**
+   * 弹框事件
+   */
+  onClickBtn: function (e) {
+    let data = this.data;
+
+    if ((data.texts.includes(data.secondWord) && !data.thirdWord)
+        || (data.texts.includes(data.thirdWord) && !data.second)
+        ) {
+      this.toast('已经生成！');
+      this.generateName();
+    } else {
+      this.toast('没有符合条件的名字!');
+    }
+  },
+  /**
+   * 再次封装弹框
+   */
+  toast: function (title, duration, successCallBack, failCallBack) {
+    let that = this;
+    wx.showToast({
+      title: title || '成功',
+      icon: '',
+      image: '',
+      duration: duration || 2000,
+      mask: true,
+      success: function(res) {
+        successCallBack && successCallBack();
+      },
+      fail: function(res) {
+        failCallBack && failCallBack();
+      },
+      complete: function(res) {},
+    })
+  },
+  /**
+   * 循环生成
+   */
+  generateName: function () {
+    let that = this;
+    let data = this.data;
+    that.setData({
+      names : data.texts.split(''),
+      showList : true
+    })
   }
 })
